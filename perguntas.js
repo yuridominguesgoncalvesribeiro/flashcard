68aa483568d3a20af2d4eb0e1cdaa3d4172ca8e3
yuridominguesgoncalvesribeiro <ddminguezz@gmail.com>
2024-11-26 00:28:15 -0300

    final

---
+++ perguntas.js
@@ -0,0 +1,53 @@
+criaCartao(
+    'Tema CSS',
+    'O que é CSS?',
+    'É uma linguagem para estilizar páginas HTML'
+)
+
+criaCartao(
+    'Tema CSS',
+    ' Para que serve o id no CSS?',
+    'Estilizar um elemento único'
+)
+
+criaCartao(
+    'Tema CSS',
+    'Como mudar a cor de fundo com CSS?',
+    'Use background-color'
+)
+
+criaCartao(
+    'Tema HTML',
+    'Para que serve a tag a?',
+    'Criar links'
+)
+
+criaCartao(
+'Tema HTML',
+    'O que é uma tag div?',
+    'Um conteiner de bloco para agrupar elementos'
+)
+
+criaCartao(
+    'Tema HTML',
+    'Como criar uma lista numerada?',
+    'Use a tag <ol> com <li>'
+)
+
+criaCartao(
+    'Tema JavaScript',
+    'O que é JavaScript?',
+    'Linguagem para adicionar interatividade em páginas web'
+)
+
+criaCartao(
+    'Tema JavaScript',
+    'Como exibir uma mensagem de alerta?',
+    'Use alert()'
+)
+
+criaCartao(
+    'Tema JavaScript',
+    'Para que serve o let no JavaScript?',
+    'Declarar variáveis com escopo de bloco'
+)
